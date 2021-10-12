3bf9470a659b1943e6edd510e974679bffa8fb81
student <bartnik.luk@gmail.com>
2021-10-12 09:33:20 +0200

    Lab1

---
+++ app/src/main/java/com/example/myfrags/Fragment1.java
@@ -0,0 +1,28 @@
+package com.example.myfrags;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+        return view;
+    }
+}
